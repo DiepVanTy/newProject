9622673135ce85e6c13fdd7f899f51acfdcd9f49
DiepVanTy <102716047+DiepVanTy@users.noreply.github.com>
2022-07-15 09:55:06 +0700

    first commit

---
+++ asset/js/main.js
@@ -0,0 +1,11 @@
+// menu_icon: begin 
+
+$(document).ready(function () {
+  $(".menu__icon").click(function () {
+    $(this).toggleClass("active");
+    $(".navigation").toggleClass("active");
+  });
+});
+// menu_icon: end
+
+

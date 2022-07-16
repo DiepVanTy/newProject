a76a6b37e6cbbd0ee55f28f6b13bd6805f052702
DiepVanTy <102716047+DiepVanTy@users.noreply.github.com>
2022-07-16 11:04:34 +0700

    third commit

--- asset/js/main.js
+++ asset/js/main.js
@@ -5,7 +5,37 @@ $(document).ready(function () {
     $(this).toggleClass("active");
     $(".navigation").toggleClass("active");
   });
+  // menu_icon: end
+
 });
-// menu_icon: end
+  // tab : begin.
+function tabShow(element, classNameOfTabContent) {
+  let tab_btns = document.querySelectorAll(".tab__buttons .button");
+  let tab_content = document.querySelectorAll(".tab__content");
+  for (let i = 0; i < tab_btns.length; i++) {
+    tab_btns[i].classList.remove("active");
+    tab_content[i].classList.remove("is-show");
+    element.classList.add("active");
+    document
+      .getElementsByClassName(classNameOfTabContent)[0]
+      .classList.add("is-show");
+  }
+}
+  // tab : end.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 

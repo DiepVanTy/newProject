d8bc9d115de00988692294b9a30e9669936eeb24
DiepVanTy <102716047+DiepVanTy@users.noreply.github.com>
2022-07-23 14:03:23 +0700

    forth commit

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,4 +1,19 @@
-// menu_icon: begin 
+// toggle header: begin
+(function toggleHeader() {
+  let prevScrollpos = window.pageYOffset;
+  window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("header").style.transform = "translateY(0)";
+    } else {
+      document.getElementById("header").style.transform = "translateY(-100%)";
+    }
+    prevScrollpos = currentScrollPos;
+  };
+})();
+// toggle header: end
+
+// menu_icon: begin
 
 $(document).ready(function () {
   $(".menu__icon").click(function () {
@@ -6,9 +21,8 @@ $(document).ready(function () {
     $(".navigation").toggleClass("active");
   });
   // menu_icon: end
-
 });
-  // tab : begin.
+// tab : begin.
 function tabShow(element, classNameOfTabContent) {
   let tab_btns = document.querySelectorAll(".tab__buttons .button");
   let tab_content = document.querySelectorAll(".tab__content");
@@ -21,21 +35,29 @@ function tabShow(element, classNameOfTabContent) {
       .classList.add("is-show");
   }
 }
-  // tab : end.
+// tab : end.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// silde banner:begin
+let bannerImage = () => {
+  let index = 0;
+  setTimeout(slideBanner, 1);
+  function slideBanner() {
+    let slides = document.querySelectorAll(".banner__right .banner__slide");
+    let textLarge = document.querySelectorAll(".banner__slide .text--large");
+    let textSmall = document.querySelectorAll(".banner__slide .text--small");
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.opacity = 0;
+      textLarge[i].classList.remove("translate");
+      textSmall[i].classList.remove("translate");
+    }
+    slides[index].style.opacity = 1;
+    textLarge[index].classList.add("translate");
+    textSmall[index].classList.add("translate");
+    setTimeout(slideBanner, 3000);
+    index++;
+    if (index == slides.length) {
+      index = 0;
+    }
+  }
+};
+bannerImage();
